64b3718ff17bc4613c0c8a109dba1550fdfb8d90
Ashish <ashish_mohanty10@yahoo.com>
2022-07-28 23:41:06 +0530

    updated customer rest controller

--- src/main/java/com/love2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/love2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +15,28 @@ import com.love2code.springdemo.service.CustomerService;
 @RequestMapping("/api")
 public class CustomerRestController {
 
+	// autowire CustomerService
 	@Autowired
 	private CustomerService customerService;
 	
+	// Add mapping for GET / customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {
 		
 		return customerService.getCustomers();
+	}
+	
+	// Add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		
+		Customer customer = customerService.getCustomer(customerId);
+		
+		if(customer == null) {
+			
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		return customer;	
 	}
 }
